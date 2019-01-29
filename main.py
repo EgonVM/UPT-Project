6bf2fe6368dbcc4efc1aa9e911dd0e8e140352ab
EgonVM <45141554+EgonVM@users.noreply.github.com>
2019-01-30 00:12:25 +0200

    Laying down the fountation
    Added placeholder main file and added folders.

---
+++ main.py
@@ -0,0 +1,7 @@
+#Tere!
+#See on placeholder fail.
+#Faile teen keeles Python moodulitega Pygame, Pickle, Random.
+#Loodetavasti koostöö sujub kenasti.
+
+print("Edasilükatud mäng on lõpuks hea, kuid kiirustatud mäng on igavesti halb.")
+print("-Shigeru Miyamoto")
