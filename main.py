c9f9c89a46ae3cd536003608525cb6f09902eb0b
EgonVM <45141554+EgonVM@users.noreply.github.com>
2019-09-07 00:04:16 +0300

    Esimesed sammud
    Alustasin main faili programmeerimist.

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
-#Tere!
-#See on placeholder fail.
-#Faile teen keeles Python moodulitega Pygame, Pickle, Random.
-#Loodetavasti koostöö sujub kenasti.
+from pygame import *
+import random
+import pickle
 
-print("Edasilükatud mäng on lõpuks hea, kuid kiirustatud mäng on igavesti halb.")
-print("-Shigeru Miyamoto")
+game = True
+mode = 1
+try:
+    aken.blit(error) #Kuna ma ei taha, et try passiga ignoreeriks praegusel hetkel excepti, siis kirjutan siia valesti kirjutatud funktsiooni.
+except:
+    window_width = 300
+    window_height = 500
+    fullscreen = False
+init()
+if fullscreen:
+    pass
+if not fullscreen:
+    window = display.set_mode([window_width, window_height])
+while game:
+    try:
+        while game:
+            while mode == 1 and game:
+                for i in event.get():
+                    if i.type == QUIT:
+                        game = False
+    except:
+        game = False
+quit()
